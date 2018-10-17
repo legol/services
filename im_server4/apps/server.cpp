e0caf821ea2f1da5be6f80d1ed408a6ce6cbe473
ubuntu <legol.chen@gmail.com>
2018-10-16 22:42:54 -0700

    Use class method as callback

--- im_server4/apps/server.cpp
+++ im_server4/apps/server.cpp
@@ -5,30 +5,61 @@
 #include "framed_transport.h"
 #include "server_thread.h"
 
-int32_t onPacketReceived(std::shared_ptr<FramedPacket> packet) {
-  return 0;
-}
+// Use functions as callback
+// int32_t onPacketReceived(std::shared_ptr<FramedPacket> packet) {
+//   printf("%s:%d:%s\n", __FILE__, __LINE__, __FUNCTION__);
+//   return 0;
+// }
+//
+// int32_t onConnected(int32_t fd, sockaddr_in &addr) {
+//   printf("%s:%d:%s\n", __FILE__, __LINE__, __FUNCTION__);
+//   return 0;
+// }
+//
+// void onDisconnected(int32_t fd) {
+//   printf("%s:%d:%s\n", __FILE__, __LINE__, __FUNCTION__);
+// }
 
-int32_t onConnected(int32_t fd, sockaddr_in &addr) {
-  printf("%s:%s\n", __FILE__, __FUNCTION__);
-  return 0;
-}
+// Use class method as callback:
+class TestServer {
+public:
+  int32_t onPacketReceived(std::shared_ptr<FramedPacket> packet) {
+    printf("%s:%d:%s\n", __FILE__, __LINE__, __FUNCTION__);
+    return 0;
+  }
 
-void onDisconnected(int32_t fd) {
-  printf("%s:%s\n", __FILE__, __FUNCTION__);
-}
+  int32_t onConnected(int32_t fd, sockaddr_in &addr) {
+    printf("%s:%d:%s\n", __FILE__, __LINE__, __FUNCTION__);
+    return 0;
+  }
+
+  void onDisconnected(int32_t fd) {
+    printf("%s:%d:%s\n", __FILE__, __LINE__, __FUNCTION__);
+  }
+};
+
+using namespace std::placeholders;
 
 int main()
 {
-  std::shared_ptr<ITransport> framed_transport = FramedTransport::create(
-      std::function<int32_t(std::shared_ptr<FramedPacket>)>(onPacketReceived),
-      std::function<int32_t(int32_t fd, sockaddr_in & addr)>(onConnected),
-      std::function<void(int32_t)>(onDisconnected));
+  // Use functions as callback
+  // std::shared_ptr<ITransport> framed_transport = FramedTransport::create(
+  //     std::function<int32_t(std::shared_ptr<FramedPacket>)>(onPacketReceived),
+  //     std::function<int32_t(int32_t fd, sockaddr_in & addr)>(onConnected),
+  //     std::function<void(int32_t)>(onDisconnected));
 
-  std::shared_ptr<ServerThread> server_thread =
-      ServerThread::create("192.168.1.171", 22334, framed_transport);
+  // Use class method as callback
+  TestServer server;
+  std::shared_ptr<ITransport> framedTransport = FramedTransport::create(
+      std::bind(&TestServer::onPacketReceived, &server, _1),
+      std::bind(&TestServer::onConnected, &server, _1, _2),
+      std::bind(&TestServer::onDisconnected, &server, _1));
 
-  server_thread->startAndJoin();
+
+  std::shared_ptr<ServerThread> serverThread =
+      ServerThread::create("192.168.101.116", 22334, framedTransport);
+
+  serverThread->startAndJoin();
 
   // int x = 9;
   // std::thread threadObj([] {
--- im_server4/apps/test_client.cpp
+++ im_server4/apps/test_client.cpp
@@ -1,33 +1,64 @@
 #include <iostream>
 #include <thread>
 #include <memory>
+#include <functional>
 
 #include "framed_transport.h"
 #include "client_thread.h"
 
-int32_t onPacketReceived(std::shared_ptr<FramedPacket> packet) {
-  return 0;
-}
+// Use functions as callback:
+// int32_t onPacketReceived(std::shared_ptr<FramedPacket> packet) {
+//   return 0;
+// }
+//
+// int32_t onConnected(int32_t fd, sockaddr_in &addr) {
+//   printf("%s:%s\n", __FILE__, __FUNCTION__);
+//   return 0;
+// }
+//
+// void onDisconnected(int32_t fd) {
+//   printf("%s:%s\n", __FILE__, __FUNCTION__);
+// }
 
-int32_t onConnected(int32_t fd, sockaddr_in &addr) {
-  printf("%s:%s\n", __FILE__, __FUNCTION__);
-  return 0;
-}
+// Use class method as callback:
+class TestClient {
+public:
+  int32_t onPacketReceived(std::shared_ptr<FramedPacket> packet) {
+    printf("%s:%d:%s\n", __FILE__, __LINE__, __FUNCTION__);
+    return 0;
+  }
 
-void onDisconnected(int32_t fd) {
-  printf("%s:%s\n", __FILE__, __FUNCTION__);
-}
+  int32_t onConnected(int32_t fd, sockaddr_in &addr) {
+    printf("%s:%d:%s\n", __FILE__, __LINE__, __FUNCTION__);
+
+    return 0;
+  }
+
+  void onDisconnected(int32_t fd) {
+    printf("%s:%d:%s\n", __FILE__, __LINE__, __FUNCTION__);
+  }
+};
+
+using namespace std::placeholders;
 
 int main() {
-  std::shared_ptr<ITransport> framed_transport = FramedTransport::create(
-      std::function<int32_t(std::shared_ptr<FramedPacket>)>(onPacketReceived),
-      std::function<int32_t(int32_t fd, sockaddr_in &addr)>(onConnected),
-      std::function<void(int32_t)>(onDisconnected));
+  // Use functions as callback:
+  // std::shared_ptr<ITransport> framed_transport = FramedTransport::create(
+  //     std::function<int32_t(std::shared_ptr<FramedPacket>)>(onPacketReceived),
+  //     std::function<int32_t(int32_t fd, sockaddr_in &addr)>(onConnected),
+  //     std::function<void(int32_t)>(onDisconnected));
 
-  std::shared_ptr<ClientThread> client_thread =
-      ClientThread::create("192.168.1.171", 22334, framed_transport);
+  // Use class method as callback:
+  TestClient client;
+  std::shared_ptr<ITransport> framedTransport = FramedTransport::create(
+      std::bind(&TestClient::onPacketReceived, &client, _1),
+      std::bind(&TestClient::onConnected, &client, _1, _2),
+      std::bind(&TestClient::onDisconnected, &client, _1));
 
-  client_thread->startAndJoin();
+  std::shared_ptr<ClientThread> clientThread =
+      ClientThread::create("192.168.101.116", 22334, framedTransport);
+
+  clientThread->startAndJoin();
 
   return 0;
 }
